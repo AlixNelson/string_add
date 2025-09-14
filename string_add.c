bba212a8a42a950331555680859a03a213b9aa20
AlixNelson <2664732230@qq.com>
2025-09-14 21:53:30 +0800

    虽然不知道为什么但就是在把位加法逻辑放到循环中就可以正常加任意位int了，但还没有实现最高位进位，并且没法处理int大于数字字符串的情况

--- string_add.c
+++ string_add.c
@@ -12,37 +12,49 @@
 // 但优势在于可以处理无限长的数
 // 并且不需要对输入进行类型转换
 
+// 目前只能加一位int，还没有想好怎么处理标准int
+
 char testing_num_string[] = "123456789"; // 测试用例，兼顾了必须进位和过于巨大两个特征
 
-
-char string_add_by_place(char raw, int adding, bool *carry) { // 按位进行加，将在主程序中递归使用
+/*
+char string_add_by_place(char raw, int adding, int *carry) { // 按位进行加，将在主程序中递归使用
     int digit = raw - '0';
-    digit += adding;
-    *carry = (digit >= 10);
+    digit += adding%10;
+    *carry = (digit / 10);
     if (*carry) {
         digit -= 10;
     }
     return digit + '0';
 }
+*/
 
 void string_add_main(char *num_string, int adding) {
     
     int len = strlen(num_string);
-    int add = adding;
-    bool carry = false;
-    for (int i = len - 1; i >= 0; i--) { // 从低位遍历调用按位加 
-        num_string[i] = string_add_by_place(num_string[i], add, &carry);
-        if (carry) {
-            add = 1;
-        } else {
-            add = 0;
-            break;
-        }
+    int i = len - 1;
+    /*
+    num_string[i] = string_add_by_place(num_string[i], adding, &carry);
+    i--;
+    */
+    while (adding > 0 && i >= 0) {
+        /*
+        int carry_num = carry;
+        carry = 0;
+        num_string[i] = string_add_by_place(num_string[i], carry_num, &carry);
+        i--;
+        */
+       int digit = num_string[i] - '0';
+       digit += adding;
+       adding = digit / 10;
+       digit %= 10;
+       num_string[i] = digit + '0';
+       i--;
     }
+    // 如果最高位需要进位则应该进行内存动态分配并进行最后一次进位
 }
 
 int main(void) {
-    string_add_main(testing_num_string, 3);
+    string_add_main(testing_num_string, 34);
     printf("%s", testing_num_string);
     system("pause");
     return 0;
